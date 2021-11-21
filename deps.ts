37beafcfc2ba8fcbb1d3e1e852efec7dd6d8a70e
Simon Haugen <smn.hgn@gmail.com>
2021-11-21 18:02:07 +0100

    added project

---
+++ deps.ts
@@ -0,0 +1,4 @@
+export { parse as parseArgs } from "https://deno.land/std@0.115.1/flags/mod.ts";
+export * as log from "https://deno.land/std@0.115.1/log/mod.ts";
+export { readLines } from "https://deno.land/std@0.115.1/io/mod.ts";
+export * as path from "https://deno.land/std@0.115.1/path/mod.ts";
---
+++ index.ts
@@ -0,0 +1,85 @@
+import { parseArgs, log } from "./deps.ts";
+
+interface ParsedRow {
+  [header: string]: string | number;
+}
+
+const usage = `
+Usage: csv-json-cli -f /path/to/csv -o /path/to/json
+    
+Optional: 
+  -s <separator> 
+  -p <pretty json>
+`;
+
+function printHelp() {
+  log.warning(usage);
+}
+
+function isNumberString(value: string): boolean {
+  return /^(\d+\.)?\d+/g.test(value);
+}
+
+async function main(args: string[]) {
+  const {
+    help,
+    f: file,
+    o: output,
+    s: separator = ",",
+    pretty,
+  } = parseArgs(args);
+
+  if (help) {
+    printHelp();
+    Deno.exit();
+  }
+
+  if (typeof output !== "string" || !output.endsWith(".json")) {
+    log.error("Please specify output");
+    printHelp();
+    Deno.exit(1);
+  }
+
+  let content;
+
+  try {
+    content = await Deno.readTextFile(file);
+  } catch (error) {
+    log.error(error);
+    Deno.exit(1);
+  }
+
+  const [headerRow, ...rows] = content.split("\n");
+  const headers = headerRow.split(separator);
+
+  const parsedRows: ParsedRow[] = [];
+
+  for (const row of rows) {
+    const columns = row.split(separator);
+    const parsedRow: ParsedRow = {};
+
+    for (let columnIndex = 0; columnIndex < columns.length; columnIndex++) {
+      const columnHeader = headers[columnIndex];
+      const columnData = isNumberString(columns[columnIndex])
+        ? parseFloat(columns[columnIndex])
+        : columns[columnIndex];
+
+      parsedRow[columnHeader] = columnData;
+    }
+
+    parsedRows.push(parsedRow);
+  }
+
+  try {
+    const json = pretty
+      ? JSON.stringify(parsedRows, null, 2)
+      : JSON.stringify(parsedRows);
+
+    await Deno.writeTextFile(output, json);
+  } catch (error) {
+    log.error(error);
+    Deno.exit(1);
+  }
+}
+
+await main(Deno.args);
