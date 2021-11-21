d215ab36630829b47b69e60f0ee8e2b5825da47e
Simon Haugen <smn.hgn@gmail.com>
2021-11-21 18:16:14 +0100

    removed unused dependencies

--- deps.ts
+++ deps.ts
@@ -1,4 +1,2 @@
 export { parse as parseArgs } from "https://deno.land/std@0.115.1/flags/mod.ts";
 export * as log from "https://deno.land/std@0.115.1/log/mod.ts";
-export { readLines } from "https://deno.land/std@0.115.1/io/mod.ts";
-export * as path from "https://deno.land/std@0.115.1/path/mod.ts";
